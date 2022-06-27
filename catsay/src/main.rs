371a732bf545c665d6416c8f26ae7876f98dd9a2
mihail.gaberov <mihail.gaberov@ysura.com>
2022-06-27 10:41:32 +0200

    👨🏻‍🎓 Introduced colors. Saving to file

--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -1,18 +1,34 @@
 extern crate structopt;
 use structopt::StructOpt;
+extern crate colored;
+use colored::*;
 
 #[derive(StructOpt)]
 struct Options {
     #[structopt(default_value="Meow!")]
-    message: String
+    message: String,
+    #[structopt(short = "d", long = "dead")]
+    /// Make the cat appear dead
+    dead: bool,
+    #[structopt(short = "f", long = "file", parse(from_os_str))]
+    /// Load the cat picture from the specified file
+    catfile: Option<std::path::PathBuf>,
 }
 fn main() {
     let options = Options::from_args();
     let message = options.message;
-    println!("{}",message);
+
+    if message.to_lowercase() == "woof" {
+        eprintln!("A cat shouldn't bark like a dog.")
+    }
+
+    let eye = if options.dead { "x" } else { "o" };
+
+    println!("{}", message.bright_yellow().underline()
+        .on_purple());
     println!(" \\");
-    println!("\\");
-    println!("/\\_/\\");
-    println!("( o o )");
-    println!("=( I )=");
+    println!("  \\");
+    println!("    /\\_/\\");
+    println!("   ( {eye} {eye} )", eye=eye.red().bold());
+    println!("   =( I )=");
     }
