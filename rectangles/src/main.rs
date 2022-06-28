ea4736f4d345adf86171f05c0b21826c2a92baf8
mihail.gaberov <mihail.gaberov@ysura.com>
2022-06-28 17:21:08 +0200

    :student: Multiple impl Blocks

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,11 +8,14 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+}
 
+impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
 }
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
