7e2c27e5bf7c1e6552a723ce7ba6cf08673dd957
mihail.gaberov <mihail.gaberov@ysura.com>
2022-06-28 16:35:15 +0200

    :student: dbg!

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -5,17 +5,11 @@ struct Rectangle {
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 30,
+        width: dbg!(30 * scale),
         height: 50,
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    dbg!(&rect1);
 }
