c4af10b8b4f64c211ebd23e55de7b3546383f259
mihailgaberov <mihail.gaberov@gmail.com>
2022-07-02 12:17:54 +0200

    :student: Re-exporting Names with pub use

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -14,6 +14,11 @@ mod front_of_house {
     }
 }
 mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
     pub struct Breakfast {
         pub toast: String,
         seasonal_fruit: String,
@@ -29,6 +34,8 @@ mod back_of_house {
     }
 }
 
+pub use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     // Order a breakfast in the summer with Rye toast
     let mut meal = back_of_house::Breakfast::summer("Rye");
@@ -38,5 +45,10 @@ pub fn eat_at_restaurant() {
 
     // The next line won't compile if we uncomment it; we're not allowed
     // to see or modify the seasonal fruit that comes with the meal
-    meal.seasonal_fruit = String::from("blueberries");
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+    hosting::add_to_waitlist();
 }
