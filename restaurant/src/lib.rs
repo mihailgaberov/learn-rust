e460a53ed4ddb7915419aaf76c75281751e22712
Mihail Gaberov <mihail.gaberov@gmail.com>
2022-07-03 09:36:41 +0200

    :student: Separating modules into different files

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,18 +1,5 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+mod front_of_house;
 
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
 mod back_of_house {
     pub enum Appetizer {
         Soup,
