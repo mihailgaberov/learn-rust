d2a86ad0c6963a4371d20f25dc4d5649a64b1673
mihail.gaberov <mihail.gaberov@ysura.com>
2022-07-06 09:43:42 +0200

    :student: Storing UTF-8 Encoded Text with Strings

---
+++ strings/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+
+    println!("s: {}", s);
+}
