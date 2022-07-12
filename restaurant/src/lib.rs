6b73da0d355f30ead5e771daadccdf6040c9b0a7
Mihail Gaberov <mihail.gaberov@ysura.com>
2022-07-12 10:48:28 +0200

    Update lib.rs

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -30,10 +30,6 @@ pub fn eat_at_restaurant() {
     meal.toast = String::from("Wheat");
     println!("I'd like {} toast please", meal.toast);
 
-    // The next line won't compile if we uncomment it; we're not allowed
-    // to see or modify the seasonal fruit that comes with the meal
-    // meal.seasonal_fruit = String::from("blueberries");
-
     let order1 = back_of_house::Appetizer::Soup;
     let order2 = back_of_house::Appetizer::Salad;
 
