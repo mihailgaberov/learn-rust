d57533a352b3bb5bfdad6e673b4014692f06f252
Mihail Gaberov <mihail.gaberov@gmail.com>
2022-07-17 10:48:05 +0200

    :student: Separating modules into different files

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -26,6 +26,7 @@ pub use crate::front_of_house::hosting;
 pub fn eat_at_restaurant() {
     // Order a breakfast in the summer with Rye toast
     let mut meal = back_of_house::Breakfast::summer("Rye");
+    
     // Change our mind about what bread we'd like
     meal.toast = String::from("Wheat");
     println!("I'd like {} toast please", meal.toast);
