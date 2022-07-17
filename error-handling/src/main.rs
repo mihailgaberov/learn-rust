e0b638a80c6eb9b9f2df5bd6575f9a2425c98661
Mihail Gaberov <mihail.gaberov@gmail.com>
2022-07-17 11:25:17 +0200

    :student: Error handling

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,7 +1,21 @@
+
+#![allow(unused)]
 fn main() {
-    use std::net::IpAddr;
+pub struct Guess {
+    value: i32,
+}
 
-    let home: IpAddr = "127.0.0.1".parse().unwrap();
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
 
-    println!("Home address is: {}", home);
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
 }
