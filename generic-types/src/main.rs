20594f71a132bc284498146343f270154dfe1e17
mihail.gaberov <mihail.gaberov@ysura.com>
2022-07-18 09:16:25 +0200

    :student: Generic types - removing duplication by extracting a function

--- generic-types/src/main.rs
+++ generic-types/src/main.rs
@@ -1,14 +1,25 @@
-fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
 
-    let mut largest = number_list[0];
-
-    for number in number_list {
-        if number > largest {
-            largest = number;
+    for &item in list {
+        if item > largest {
+            largest = item;
         }
     }
 
-    println!("The largest number is {}", largest);
-    assert_eq!(largest, 100);
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+    assert_eq!(result, 100);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+    assert_eq!(result, 6000);
 }
