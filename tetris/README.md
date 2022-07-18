205f5f52a199d6fdafc37926b18808eff72fa8f5
mihail.gaberov <mihail.gaberov@ysura.com>
2022-07-18 13:15:32 +0200

    ✨ Add README, update structs

---
+++ tetris/README.md
@@ -0,0 +1,3 @@
+## Let's code Tetris with Rust, WASM and React
+
+https://www.youtube.com/watch?v=_lAr7JveRVE
--- tetris/src/tetris.rs
+++ tetris/src/tetris.rs
@@ -1,12 +1,15 @@
+use std::collections:HashSet;
 
+pub struct Pos(pub i32, pub i32)
 
 pub strict Shape {
-  positions: HashSet<(i32, i32)>,
+  positions: HashSet<Pos>,
 
 }
 
 pub struct Tetris {
   width: usize,
   height: usize,
-  
+  current_shape: Shape,
+  fixed_shapes: Vec<Shape>
 }
