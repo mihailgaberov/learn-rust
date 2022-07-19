9cd0a6f856ae47f3331f80c8b0b6a2e6afb48c51
mihail.gaberov <mihail.gaberov@ysura.com>
2022-07-19 09:58:48 +0200

    ✨ Add constructor

--- tetris/src/tetris.rs
+++ tetris/src/tetris.rs
@@ -1,15 +1,24 @@
-use std::collections:HashSet;
-
-pub struct Pos(pub i32, pub i32)
-
-pub strict Shape {
-  positions: HashSet<Pos>,
-
+use std::collections::HashSet;
+#[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
+pub struct Pos(pub i32, pub i32);
+#[derive(Debug, Clone)]
+pub struct Shape {
+    positions: HashSet<Pos>,
+}
+pub struct Tetris {
+    width: usize,
+    height: usize,
+    current_shape: Shape,
+    fixed_shapes: Vec<Shape>,
 }
 
-pub struct Tetris {
-  width: usize,
-  height: usize,
-  current_shape: Shape,
-  fixed_shapes: Vec<Shape>
+impl Tetris {
+    pub fn new(width: usize, height: usize) -> Self {
+        Self {
+            width,
+            height,
+            current_shape: todo!(),
+            fixed_shapes: vec![],
+        }
+    }
 }
