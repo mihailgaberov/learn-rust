2a30a7c4bd2153d90c9988bb6e55be6c4316abf7
Mihail Gaberov <mihail.gaberov@gmail.com>
2022-07-29 13:37:48 +0200

    :student: Generic Type Parameters, Trait Bounds, and Lifetimes Together

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -13,7 +13,7 @@ fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a st
 where
     T: Display,
 {
-    println!("Announcement! {}", ann);
+    println!("Announcement: {}", ann);
     if x.len() > y.len() {
         x
     } else {
