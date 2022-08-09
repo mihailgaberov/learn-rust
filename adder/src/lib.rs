71aab68d4b0bba87487da2ddbc0dbaef0985b4a0
mihail.gaberov <mihail.gaberov@ysura.com>
2022-08-09 11:43:16 +0200

    :student: How to Write Tests

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -10,6 +10,10 @@ impl Rectangle {
     }
 }
 
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -40,5 +44,16 @@ mod tests {
         };
 
         assert!(!smaller.can_hold(&larger));
+    }
+
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
     }
 }
