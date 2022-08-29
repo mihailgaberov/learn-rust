1e5ed397f0f4e88ab36db60f0181defdf0f12be8
mihail.gaberov <mihail.gaberov@ysura.com>
2022-08-29 09:06:14 +0200

    :student: Controlling how tests are run

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -75,4 +75,25 @@ mod tests {
     fn it_adds_two() {
         assert_eq!(4, add_two(2));
     }
+
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        // code that takes an hour to run
+    }
 }
