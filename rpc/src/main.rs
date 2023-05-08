9502e072abb23e084e7c45a57bb3fbb90fb6dc47
Mihail Gaberov <mihail.gaberov@gmail.com>
2023-05-08 22:39:08 +0200

    :student: Send a non-blocking rpc from Rust to Boringtun API

--- rpc/src/main.rs
+++ rpc/src/main.rs
@@ -1,4 +1,4 @@
-use serde::{Deserialize, Serialize};
+/* use serde::{Deserialize, Serialize};
 use surf::Client;
 
 #[derive(Serialize, Deserialize)]
@@ -9,12 +9,17 @@ struct Response {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let client = Client::new();
-    let url = "http://localhost:8080"; // replace with your Boringtun API endpoint
+    let url = "https://rpc.testnet.near.org";
+    // let jsonrpc = "2.0";
+    let id = "dev-1668546365877-28764765342385";
+    let method = "query";
+    let params = r#"{"request_type": "call_function", "finality": "final", "account_id": "dev-1668546365877-28764765342385", "method_name": "nft_metadata", "args_base64": "e30="}"#;
+
 
     let request_body = serde_json::json!({
-        "method": "ping",
-        "params": [],
-        "id": 1,
+        "method": method,
+        "params": params,
+        "id": id,
     });
 
     let response = client
@@ -25,5 +30,43 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     println!("{}", response.message);
 
+    Ok(())
+} */
+
+use std::error::Error;
+use serde::{ Deserialize, Serialize };
+
+#[derive(Serialize, Deserialize)]
+struct RpcRequest {
+    jsonrpc: String,
+    method: String,
+    params: String,
+    id: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct RpcResponse {
+    result: Option<String>,
+}
+
+async fn call_rpc() -> Result<(), Box<dyn Error>> {
+    let url = "https://rpc.testnet.near.org";
+    let params =
+        r#"{"request_type": "call_function", "finality": "final", "account_id": "dev-1668546365877-28764765342385", "method_name": "nft_metadata", "args_base64": "e30="}"#;
+    let request = RpcRequest {
+        jsonrpc: "2.0".to_owned(),
+        method: "query".to_owned(),
+        params: params.to_string(),
+        id: "dev-1668546365877-28764765342385".to_owned(),
+    };
+    let mut res = surf::post(url).body(surf::Body::from_json(&request)?).await?;
+    let response: RpcResponse = res.body_json().await?;
+    println!("RPC response: {:?}", response);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    call_rpc().await?;
     Ok(())
 }
